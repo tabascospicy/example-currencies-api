1bc045765f8b26613a33912771869191426fc7f0
rafaelTabasca <rafael@capicua.com.uy>
2024-02-05 21:26:05 -0400

    Proyect starting

---
+++ cmd/currencies/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+
+
+func main(){
+	// Run the server
+	// server.Run()
+	fmt.Println("Hello, World!")
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/spicyt/currencies
+
+go 1.21.6
